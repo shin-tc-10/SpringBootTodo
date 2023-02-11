d375853b8931d925f6c8c1e5e78ffb135e63688e
shin-nkt-09 <shin.nkt.09@gmail.com>
2023-02-11 13:16:37 +0900

    メインクラス読み込みエラーのためプロジェクト再作成

---
+++ sbTodo/src/main/java/com/example/sbTodo/SbTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTodoApplication.class, args);
+	}
+
+}
---
+++ sbTodo/src/test/java/com/example/sbTodo/SbTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
