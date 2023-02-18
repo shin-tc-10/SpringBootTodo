462c073529e0fc67d1596afe182ac52df6b1db8b
shin-nkt-09 <shin.nkt.09@gmail.com>
2023-02-18 12:02:47 +0900

    リポジトリ作成

--- sbTodo/src/main/java/com/example/sbTodo/domain/todo/TodoRepository.java
+++ sbTodo/src/main/java/com/example/sbTodo/domain/todo/TodoRepository.java
@@ -1,5 +1,20 @@
 package com.example.sbTodo.domain.todo;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public class TodoRepository {
 
+	@Select("SELECT * FROM TODOS")
+	List<TodoEntity> selectAll();
+
+	@Insert("INSERT INTO TODOS(TASK, DETAIL)VALUES(#{TASK}, #{DETAIL}))")
+	void insert(String task, String detail);
+
+	@Select("SELECT * FROM TODOS WHERE ID = #{todoId}")
+	TodoEntity selectById();
 }
--- sbTodo/src/main/java/com/example/sbTodo/web/todo/TodoForm.java
+++ sbTodo/src/main/java/com/example/sbTodo/web/todo/TodoForm.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class TodoForm {
 
+	private long id;
+
 	private String task;
 
 	private String detail;
