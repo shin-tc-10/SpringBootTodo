61bdb21650ff04bff72c529a05a34a1378af9815
shin-nkt-09 <shin.nkt.09@gmail.com>
2023-02-19 14:04:45 +0900

    エンティティフィールド作成

--- sbTodo/src/main/java/com/example/sbTodo/domain/todo/TodoEntity.java
+++ sbTodo/src/main/java/com/example/sbTodo/domain/todo/TodoEntity.java
@@ -1,5 +1,13 @@
 package com.example.sbTodo.domain.todo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class TodoEntity {
 
+	private int id;
+	private String task;
+	private String detail;
 }
