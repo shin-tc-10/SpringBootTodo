e801fc5fbd926439863f778db8ba3556f42c2307
shin-nkt-09 <shin.nkt.09@gmail.com>
2023-03-01 22:05:52 +0900

    バリデーション追加

--- sbTodo/src/main/java/com/example/sbTodo/web/todo/TodoForm.java
+++ sbTodo/src/main/java/com/example/sbTodo/web/todo/TodoForm.java
@@ -1,5 +1,6 @@
 package com.example.sbTodo.web.todo;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
@@ -8,9 +9,12 @@ import lombok.Data;
 @Data
 public class TodoForm {
 
+
 	private long id;
 
+	@Length(min=1, max=100 )
 	private String task;
 
+	@Length(min=1, max=300 )
 	private String detail;
 }
